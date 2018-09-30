a3de38eb0a81e6d768469a871b9f38de6c65af28
hadimaster <hadimaster65555@gmail.com>
2018-09-30 23:04:48 +0700

    add image in flatlist

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          name: "Hadi",
+          image: "http://i.pravatar.cc/150?img=1"
+        },
+        {
+          name: "Master",
+          image: "http://i.pravatar.cc/150?img=2"
+        },
+        {
+          name: "James",
+          image: "http://i.pravatar.cc/150?img=3"
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is hadimaster app!</Text>
+        <FlatList
+          data={this.state.data}
+          style={{width: '100%'}}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item, separators}) => (
+            <View style={{paddingVertical: 100, borderBottomWidth: 1}}>
+              <Image style={{width:150, height:150}} source={{uri: item.image}}/>
+              <Text>{item.name}</Text>
+            </View>
+          )}
+        />
       </View>
     );
   }
@@ -14,7 +45,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff0000',
+    marginTop: 50,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
